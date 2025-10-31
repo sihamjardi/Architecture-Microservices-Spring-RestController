8ad1fc35011890d84eb3c006dda45c4999220e37
Your Name <sihamjardi2@gmail.com>
2025-10-31 20:38:24 +0100

    first commit

---
+++ src/main/java/ma/rest/spring/MsBanqueApplication.java
@@ -0,0 +1,18 @@
+package ma.rest.spring;
+
+import ma.rest.spring.entities.Compte;
+import ma.rest.spring.entities.TypeCompte;
+import ma.rest.spring.repositories.CompteRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class MsBanqueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsBanqueApplication.class, args);
+    }
+}
